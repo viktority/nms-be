4e13095a092a27903f488ed880d20e9e29d354f8
chukwuka01 <victorc@hardcorebiometric.com>
2020-12-15 10:16:49 +0100

    open security

--- nms-authentication/src/main/java/com/nms/security/WebSecurity.java
+++ nms-authentication/src/main/java/com/nms/security/WebSecurity.java
@@ -17,45 +17,45 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-	private Environment environment;
-	private UsersService usersService;
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment environment;
+    private UsersService usersService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@Autowired
-	public WebSecurity(Environment environment, UsersService usersService,
+    @Autowired
+    public WebSecurity(Environment environment, UsersService usersService,
                        BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.environment = environment;
-		this.usersService = usersService;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
+        this.environment = environment;
+        this.usersService = usersService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(HttpMethod.POST, environment.getProperty("signup.url.path")).permitAll()
-				.antMatchers(HttpMethod.POST, environment.getProperty("password.reset.request.path")).permitAll()
-				.antMatchers(HttpMethod.POST, environment.getProperty("password.reset.path")).permitAll()
-				.antMatchers(environment.getProperty("jsp.url.path")).permitAll()
-				.antMatchers(HttpMethod.GET, environment.getProperty("token.activation.url.path")).permitAll()
-				.antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**", "/profile_image/**",
-						"**/swagger-resources/**", "/swagger-ui.html")
-				.permitAll().anyRequest().authenticated().and().addFilter(getAuthenticationFilter())
-				.addFilter(new AuthorizationFilter(authenticationManager(), environment, usersService))
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.headers().frameOptions().disable();
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, environment.getProperty("signup.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("password.reset.request.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("password.reset.path")).permitAll()
+                .antMatchers(environment.getProperty("jsp.url.path")).permitAll()
+                .antMatchers(HttpMethod.GET, environment.getProperty("token.activation.url.path")).permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**", "/profile_image/**",
+                        "**/swagger-resources/**", "/swagger-ui.html")
+                .permitAll().anyRequest().permitAll().and().addFilter(getAuthenticationFilter())
+                .addFilter(new AuthorizationFilter(authenticationManager(), environment, usersService))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.headers().frameOptions().disable();
+    }
 
-	private AuthenticationFilter getAuthenticationFilter() throws Exception {
-		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment,
-				authenticationManager());
-		// authenticationFilter.setAuthenticationManager(authenticationManager());
-		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
-		return authenticationFilter;
-	}
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment,
+                authenticationManager());
+        // authenticationFilter.setAuthenticationManager(authenticationManager());
+        authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+        return authenticationFilter;
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+    }
 
 }
