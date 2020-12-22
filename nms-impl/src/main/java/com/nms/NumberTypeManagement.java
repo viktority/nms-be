8c39debd2888bf43b2d02eb83c188965f0f368f8
chukwuka01 <victorc@hardcorebiometric.com>
2020-12-22 17:37:25 +0100

    modified to allow for CORS

--- nms-impl/src/main/java/com/nms/NumberTypeManagement.java
+++ nms-impl/src/main/java/com/nms/NumberTypeManagement.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(exclude = {JerseyAutoConfiguration.class, FlywayAutoConfiguration.class})
 //@EnableEurekaClient
+@ComponentScan(basePackages = {"com.nms.*"})
 public class NumberTypeManagement {
 
     public static void main(String[] args) {
